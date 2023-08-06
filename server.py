4dcd6885b98d3a841716fecc2814ed2d3bc4c5bb
sucharita08 <57571018+sucharita08@users.noreply.github.com>
2023-08-06 19:35:17 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,5 @@
+import flwr as fl
+
+fl.server.start_server(server_address="0.0.0.0:8080",
+                       config=fl.server.ServerConfig(num_rounds=3),
+                       strategy=fl.server.strategy.FedMedian())
